e265399da6085e128488ad784fd026bb86325e55
mike <mikeymaxdb@users.noreply.github.com>
2021-10-24 22:47:06 -0700

    added comments

--- main.js
+++ main.js
@@ -16,28 +16,43 @@ const flips = [
     'hFlip',
 ]
 
-let rounds
-let currentRound
+/*
+ * A round is:
+ * [0] Image index
+ * [1] If it is healthy (1) or not (0)
+ * [2] CSS classname modifiers to flip/rotate the image
+ */
+
+let rounds // Array of rounds for the test
+let currentRound // Index of current round
 let score
 
 function startGame() {
+    // Reset views
     document.getElementById('StartView').style.display = 'none'
     document.getElementById('Review').style.display = 'none'
     document.getElementById('GameView').style.display = 'flex'
 
+    // Reset test state
     rounds = []
     currentRound = -1
     score = 0
 
+    // Generate rounds
     for (let i = 0; i < NUM_ROUNDS; i += 1) {
+        // 0 is unhealthy, 1 is healthy
         const isHealthy = Math.round(Math.random())
+        // Number of images to choose from
         const limit = isHealthy ? NUM_HEALTHY : NUM_BLAST
+        // Generate an image index
         const index = Math.round(Math.random() * (limit - 1)) + 1
 
+        // Random flip or rotation
         const rotation = rotations[Math.round(Math.random() * (rotations.length - 1))]
         const flip = flips[Math.round(Math.random() * (flips.length - 1))]
         const modifier = rotation + ' ' + flip
 
+        // Build the round as definder above
         rounds.push([index, isHealthy, modifier])
     }
 
@@ -50,6 +65,7 @@ function showNextRound() {
     if (currentRound < rounds.length) {
         const round = rounds[currentRound]
         const imagePrefix = round[1] ? 'healthy_' : 'blast_'
+
         document.getElementById('TestImage').src = 'images/' + imagePrefix + round[0] + '.jpg'
         document.getElementById('TestImage').className = round[2]
     } else {
