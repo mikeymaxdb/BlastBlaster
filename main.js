fc922fbe74cd5fa4ff49619013bb7c1fb8304a0b
mike <mikeymaxdb@users.noreply.github.com>
2021-10-24 22:39:44 -0700

    added rotations and flips for more variety

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ const NUM_HEALTHY = 5
 const NUM_BLAST = 5
 const NUM_ROUNDS = 3
 
+const rotations = [
+    'rotate0',
+    'rotate90',
+    'rotate180',
+    'rotate270',
+]
+
+const flips = [
+    'noVFlip',
+    'vFlip',
+    'noHFlip',
+    'hFlip',
+]
+
 let rounds
 let currentRound
 let score
@@ -20,7 +34,11 @@ function startGame() {
         const limit = isHealthy ? NUM_HEALTHY : NUM_BLAST
         const index = Math.round(Math.random() * (limit - 1)) + 1
 
-        rounds.push([index, isHealthy])
+        const rotation = rotations[Math.round(Math.random() * (rotations.length - 1))]
+        const flip = flips[Math.round(Math.random() * (flips.length - 1))]
+        const modifier = rotation + ' ' + flip
+
+        rounds.push([index, isHealthy, modifier])
     }
 
     showNextRound()
@@ -33,6 +51,7 @@ function showNextRound() {
         const round = rounds[currentRound]
         const imagePrefix = round[1] ? 'healthy_' : 'blast_'
         document.getElementById('TestImage').src = 'images/' + imagePrefix + round[0] + '.jpg'
+        document.getElementById('TestImage').className = round[2]
     } else {
         showReview()
     }
